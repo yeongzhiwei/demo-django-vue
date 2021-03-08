fb4fc4259d5e7dd956fdb07e7383347ecfb828dc
Yeong Zhi Wei <YEONG_Zhi_Wei@tech.gov.sg>
2021-03-08 08:18:10 +0000

    add separate field for tags when creating expense record

--- zwlfinance/expense/serializers.py
+++ zwlfinance/expense/serializers.py
@@ -2,18 +2,7 @@ from rest_framework import serializers
 from expense.models import Expense, Tag
 
 
-class ExpenseSerializer(serializers.HyperlinkedModelSerializer):
-    id = serializers.IntegerField(read_only=True)
-
-    class Meta:
-        model = Expense
-        fields = (
-            'id', 'timestamp', 'amount', 'payee', 
-            'description', 'tags', 'status'
-        )
-
-
-class TagSerializer(serializers.HyperlinkedModelSerializer):
+class TagSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField(read_only=True)
     
     class Meta:
@@ -21,3 +10,23 @@ class TagSerializer(serializers.HyperlinkedModelSerializer):
         fields = [
             'id', 'name'
         ]
+
+
+class ExpenseSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(read_only=True)
+    tags = TagSerializer(many=True, read_only=True)
+    tag_names = serializers.ListField(write_only=True)
+
+    class Meta:
+        model = Expense
+        fields = (
+            'id', 'timestamp', 'amount', 'payee', 
+            'description', 'tags', 'tag_names', 'status'
+        )
+
+    def create(self, validated_data):
+        tag_names = validated_data.pop('tag_names', [])
+        tags = [Tag.objects.get_or_create(name=tag_name)[0] for tag_name in tag_names]
+        expense = Expense.objects.create(**validated_data)
+        expense.tags.set(tags)
+        return expense

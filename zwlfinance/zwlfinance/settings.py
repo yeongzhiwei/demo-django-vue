da99f77ac021298a344c53cde09489871e58453c
Yeong Zhi Wei <YEONG_Zhi_Wei@tech.gov.sg>
2021-03-11 08:58:29 +0000

    change pagination to be based on query parameter limit

--- zwlfinance/zwlfinance/settings.py
+++ zwlfinance/zwlfinance/settings.py
@@ -131,6 +131,5 @@ STATICFILES_DIRS = [
 # djangorestframework
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'DEFAULT_PAGINATION_CLASS': 'zwlfinance.pagination.StandardResultsSetPagination',
 }
